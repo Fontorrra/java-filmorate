55bf7e886d71970b3a9369187703488ab35a1f1a
Даниил <daneil50005@yandex.ru>
2023-04-05 20:41:46 +0300

    Реализовано API6

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/UserNotFoundException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import ru.yandex.practicum.filmorate.model.User;
-
 public class UserNotFoundException extends NotFoundExceptions {
     public UserNotFoundException(String message) {
         super(message);
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,14 +3,11 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.filmorate.exceptions.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
-import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
